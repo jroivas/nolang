280eb1f92103f4b9332ffbb361aa80973a3cc0ed
Jouni Roivas <jroivas@iki.fi>
2017-02-26 09:16:58 +0200

    Parse more properly

--- src/pure.cpp
+++ src/pure.cpp
@@ -20,6 +20,9 @@ int main(int argc, char **argv)
     mpc_parser_t* Args  = mpc_new("args");
     mpc_parser_t* Lexp  = mpc_new("lexp");
     mpc_parser_t* Factor  = mpc_new("factor");
+    mpc_parser_t* MethodCall  = mpc_new("methodcall");
+    mpc_parser_t* Namespacedef  = mpc_new("namespacedef");
+    mpc_parser_t* Stmt  = mpc_new("stmt");
     mpc_parser_t* Term  = mpc_new("term");
     mpc_parser_t* Pure     = mpc_new("pure");
     mpc_parser_t* NolangPure     = mpc_new("nolangpure");
@@ -28,7 +31,7 @@ int main(int argc, char **argv)
         "identifier : /[A-Za-z_][A-Za-z0-9_-]*/ ;"
         "typeident  : <identifier> ':' <identifier> ;"
         "number     : /[0-9]+/ ;"
-        "string     : /\'(\\\\.|[^\'])*\'/ | /\'(\\\\.|[^\'])*\'/ ; "
+        "string     : /\"(\\\\.|[^\"])*\"/ | /\'(\\\\.|[^\'])*\'/ ; "
         "operator   : '+' | '-' | '*' | '/' ;                  "
         "methoddef  : <pure>? <identifier> <args>? \"=>\" <body> ;"
         "paramdef   : <typeident>? (',' <typeident>)*; "
@@ -37,33 +40,19 @@ int main(int argc, char **argv)
         "term       : <factor> (('*' | '/' | \"div\" | \"mod\" | \"rem\") <factor>)*;"
         "lexp       : <term> (('+' | '-') <term>)* ; "
         "expr       : <term> | <factor> ;"
-        "body       : <lexp>* ;"
+        "methodcall : '(' <lexp>? ')';"
+        "namespacedef : <identifier> ('.' <identifier>) + <methodcall>?;"
+        "stmt       : <namespacedef> | <lexp>;"
+        "body       : <stmt>* ;"
         "pure       : \"pure\" ;"
         "nolangpure : /^/ <methoddef>* /$/ ;"
-#if 0
-    mpc_err_t* err = mpca_lang(MPCA_LANG_DEFAULT,
-        "identifier : /[A-Za-z_][A-Za-z0-9_-]*/ ;"
-        "typeident  : <identifier> ':' <identifier> ;"
-        "number     : /[0-9]+/ ;"
-        "string     : /\'(\\\\.|[^\'])*\'/ | /\'(\\\\.|[^\'])*\'/ ; "
-        "operator   : '+' | '-' | '*' | '/' ;                  "
-        "methoddef  : <identifier> \"=>\" <body> ;"
-        "paramdef   : <typeident>? (',' <typeident>)*; "
-        "args       : '(' <paramdef>? ')'; "
-        "factor     : '(' <lexp> ')' | <number> | <string> | <identifier>; "
-        "term       : <factor> (('*' | '/') <factor>)*;"
-        "lexp       : <term> (('+' | '-') <term>)* ; "
-        "expr       : <term> | <factor> ;"
-        "body       : <lexp>* ;"
-        "pure       : \"pure\" ;"
-        "nolangpure : /^/ <methoddef>* /$/ ;"
-#endif
       ,
         Identifier, TypeIdent, Number, String,
         Operator,
         MethodDef, ParamDef, Args,
         Factor, Term, Lexp,
-        Expr, Body, Pure,
+        Expr, MethodCall, Namespacedef, Stmt,
+        Body, Pure,
         NolangPure, NULL);
     // expr       : <number> | '(' <operator> <expr>+ ')' ;  
 
@@ -74,14 +63,11 @@ int main(int argc, char **argv)
     }
 
     mpc_result_t r;
-    std::cout << "AA\n";
     if (mpc_parse_contents(argv[1], NolangPure, &r)) {
       /* On Success Print the AST */
-        std::cout << "bbb\n";
       mpc_ast_print(static_cast<mpc_ast_t*>(r.output));
       mpc_ast_delete(static_cast<mpc_ast_t*>(r.output));
     } else {
-        std::cout << "ccc\n";
       /* Otherwise Print the Error */
       mpc_err_print(r.error);
       mpc_err_delete(r.error);
