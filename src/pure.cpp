5de37471d0d9b4026b2795274a05ff8bd206abb7
Jouni Roivas <jroivas@iki.fi>
2017-03-13 21:17:34 +0200

    Start codegen

--- src/pure.cpp
+++ src/pure.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <fstream>
 #include <streambuf>
+#include <map>
 
 #include <3pp/mpc/mpc.h>
+#include "puremethod.hh"
 
 std::string readFile(char *fname)
 {
@@ -14,12 +16,8 @@ std::string readFile(char *fname)
     return str + '\n';
 }
 
-int main(int argc, char **argv)
+int parsePure(std::string name, std::string data, mpc_result_t *r)
 {
-    if (argc <= 1) {
-        std::cout << "Usage: " << argv[0] << "file\n";
-        return 1;
-    }
     mpc_parser_t* Comment   = mpc_new("comment");
     mpc_parser_t* Indent   = mpc_new("indent");
     mpc_parser_t* Newline   = mpc_new("newline");
@@ -177,18 +175,20 @@ int main(int argc, char **argv)
     if (err != NULL) {
         mpc_err_print(err);
         mpc_err_delete(err);
-        exit(1);
+        return 2;
     }
 
-    mpc_result_t r;
-    std::string data = readFile(argv[1]);
     int res = 0;
-    if (mpc_parse(argv[1], data.c_str(), NolangPure, &r)) {
-      mpc_ast_print(static_cast<mpc_ast_t*>(r.output));
-      mpc_ast_delete(static_cast<mpc_ast_t*>(r.output));
+    if (mpc_parse(name.c_str(), data.c_str(), NolangPure, r)) {
+        /*
+        mpc_ast_print(static_cast<mpc_ast_t*>(r.output));
+        mpc_ast_delete(static_cast<mpc_ast_t*>(r.output));
+        */
     } else {
+        /*
         mpc_err_print(r.error);
         mpc_err_delete(r.error);
+        */
         res = 1;
     }
 
@@ -211,6 +211,134 @@ int main(int argc, char **argv)
         Stmt,
         Body, Pure, Return,
         TopLevel, NolangPure);
+
+    return res;
+}
+
+std::string lvl(int l)
+{
+    std::string res = "";
+    for (int i = 0; i < l; ++i) {
+        res += " ";
+    }
+    return res;
+}
+
+void walk(mpc_ast_t *res, int l=0)
+{
+    std::string tmp = res->contents;
+    if (tmp.length() > 0) {
+        tmp = ": '" + tmp + '\'';
+    }
+    std::cout
+        << "0x" << std::hex << long(res) << lvl(l)
+        << " " << res->tag
+        <<  tmp << "\n";
+    for (int c = 0; c < res->children_num; ++c) {
+        walk(res->children[c], l + 1);
+    }
+}
+
+std::string codegen(mpc_ast_t *tree, std::map<std::string, std::string> &methods, std::map<int, std::string> &blocks, int level=0);
+
+PureMethod* parseMethod(mpc_ast_t *tree, std::map<std::string, std::string> &methods, std::map<int, std::string> &blocks, int level)
+{
+    PureMethod *m = new PureMethod();
+    bool waitName = true;
+    bool waitBody = false;
+    for (int c = 0; c < tree->children_num; ++c) {
+        std::string tag = tree->children[c]->tag;
+        std::string cnts = tree->children[c]->contents;
+        if (waitName && tag.find("identifier") != std::string::npos) {
+            m->name = cnts;
+            waitName = false;
+        } else if (tag.find("ows") != std::string::npos) {
+            // Optional whitespace
+        } else if (!waitBody && tag.find("string") != std::string::npos && cnts == "=>") {
+            // Body should follow
+            waitBody = true;
+        } else if (waitBody && tag.find("body") != std::string::npos) {
+            m->body = codegen(tree->children[c], methods, blocks, level);
+        } else if (tag.find("newline") != std::string::npos) {
+        //} else if (cnts.length() == 0) {
+            // SKIP
+        } else {
+            std::cerr << "***ERROR: Unknown node: " << tag << ": '" << cnts << "'\n";
+        }
+    }
+    return m;
+}
+
+std::string codegen(mpc_ast_t *tree, std::map<std::string, std::string> &methods, std::map<int, std::string> &blocks, int level)
+{
+    std::string res = "";
+
+    std::string tag = tree->tag;
+    std::string cnts = tree->contents;
+    bool recurse = true;
+
+    if (tag == ">") {
+        //std::cout << "ROOT\n";
+    } else if (tag.find("methoddef") != std::string::npos) {
+        // New method
+        PureMethod *m = parseMethod(tree, methods, blocks, level);
+        (void)m;
+        recurse = false;
+        level = 0;
+    } else if (tag.find("indent") != std::string::npos) {
+        level = cnts.length();
+        //std::cout << "LVL " << level << " '" << cnts << "'\n";
+        auto item = blocks.find(level);
+        if (item == blocks.end()) {
+            blocks[level] = "";
+        }
+    } else if ((tag.find("regex") != std::string::npos && cnts.length() == 0) ||
+               (tag.find("stmt") != std::string::npos && cnts.length() == 0) ||
+               (tag.find("body") != std::string::npos && cnts.length() == 0)) {
+        // SKIP
+    } else if (tag.find("number") != std::string::npos) {
+        res += cnts;
+    } else if (tag.find("ows") != std::string::npos) {
+    } else {
+        std::cerr << "***ERROR: Unknown node: " << tag << ": '" << cnts << "'\n";
+    }
+
+    if (recurse) {
+        for (int c = 0; c < tree->children_num; ++c) {
+            res += codegen(tree->children[c], methods, blocks, level);
+        }
+    }
+
+    return res;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc <= 1) {
+        std::cout << "Usage: " << argv[0] << "file\n";
+        return 1;
+    }
+
+    mpc_result_t r;
+    std::string data = readFile(argv[1]);
+
+    int res = parsePure(argv[1], data, &r);
+    if (res == 2) {
+        exit(res);
+    }
+
+    std::map<std::string, std::string> methods;
+    std::map<int, std::string> blocks;
+    
+    if (res == 0) {
+        mpc_ast_print(static_cast<mpc_ast_t*>(r.output));
+        //walk(static_cast<mpc_ast_t*>(r.output));
+        std::cout << codegen(static_cast<mpc_ast_t*>(r.output), methods, blocks) << "\n";
+        mpc_ast_delete(static_cast<mpc_ast_t*>(r.output));
+    } else {
+        mpc_err_print(r.error);
+        mpc_err_delete(r.error);
+    }
 
     return res;
 }
