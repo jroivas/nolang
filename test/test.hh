1112ffe1e96a33ec4d3353f470ff902f6a7ea1c6
Jouni Roivas <jroivas@iki.fi>
2017-05-16 22:05:44 +0300

    Improve failing test

--- test/test.hh
+++ test/test.hh
@@ -1,26 +1,32 @@
 #pragma once
 
 #include <iostream>
+#include <sstream>
 
 static long __failures = 0;
 static long __success = 0;
 static long __tests = 0;
 
+
 #define TEST_SUITE(X) \
 int main(int argc, char**argv) {\
     (void)argc; (void)argv;\
-    X;\
-    if (__failures) std::cout << "** FAIL: " << __failures << ", "; \
+    try {\
+        X;\
+    }\
+    catch (std::string e) {\
+        std::cout << "***FAIL: " << e << "\n";\
+        return 1;\
+    }\
     std::cout << " PASS: " << __success << ", "; \
     std::cout << " TOTAL: " << __tests << "\n"; \
     return __failures;\
 }
 #define TEST_CASE(X, Y) {\
-    std::cout << "== TEST " << #X << ": "; \
+    std::cout << "== TEST " << #X << "\n"; \
     Y;\
-    std::cout << "\n"; \
 }
 #define TEST_ONE_ASSERT bool only_one_assert_per_test_case; (void)(only_one_assert_per_test_case);
-#define TEST_ASSERT_TRUE(X) TEST_ONE_ASSERT do { __tests++; if ((X)) { __success++; std::cout << "SUCCESS"; } else { __failures++; std::cout << "FAIL"; } } while (0);
+#define TEST_ASSERT_TRUE(X) TEST_ONE_ASSERT do { __tests++; if ((X)) { __success++; } else { __failures++; std::stringstream s; s << #X; throw s.str(); } } while (0);
 #define TEST_ASSERT_FALSE(X) TEST_ASSERT_TRUE(!X)
-#define TEST_ASSERT_EQUALS(X, Y) TEST_ONE_ASSERT do { __tests++; if ((X == Y)) { __success++; std::cout << "SUCCESS"; } else { __failures++; std::cout << "FAIL: " << #X << " != " << #Y << ", got " << X ; } } while (0);
+#define TEST_ASSERT_EQUALS(X, Y) TEST_ONE_ASSERT do { __tests++; if ((X == Y)) { __success++; } else { __failures++; std::stringstream s; s << #X << " != " << #Y << ", got " << X; throw s.str(); } } while (0);
