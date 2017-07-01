5ba7d33d4d3f932d902d31c1008b37a5b1376ece
Jouni Roivas <jroivas@iki.fi>
2017-07-02 00:44:32 +0300

    Major refactoring

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -1,5 +1,4 @@
 #include "compiler.hh"
-#include "tools.hh"
 #include "parsers/assignmentparser.hh"
 #include "parsers/constparser.hh"
 #include "parsers/importparser.hh"
@@ -14,74 +13,215 @@
 
 using namespace nolang;
 
-std::vector<Statement*> Compiler::appendStatement(std::vector<Statement*> rdata, Statement *s)
+std::vector<Statement*> Compiler::appendStatement(std::vector<Statement*> rdata3, Statement *s)
 {
-    rdata.push_back(s);
-    return rdata;
+    rdata3.push_back(s);
+    return rdata3;
 }
 
-std::vector<Statement*> Compiler::appendBlock(std::vector<Statement*> rdata)
+std::vector<Statement*> Compiler::appendBlock(std::vector<Statement*> rdata3)
 {
-    m_blocks.push_back(rdata);
+    parent->m_blocks.push_back(rdata3);
     return std::vector<Statement*>();
 }
 
-std::vector<Statement*> Compiler::codegenRecurse(mpc_ast_t *tree, PureMethod *m, int level)
+std::vector<Statement*> Compiler::codegenRecurse(int lvl)
 {
-    std::vector<Statement*> rdata;
-    iterateTree(tree, [&] (mpc_ast_t *item) {
-        std::vector<Statement*> st = codegen(item, m, level);
+    std::vector<Statement*> rdata2;
+    iterateTree(tree, [&] (mpc_ast_t *sub) {
+        std::vector<Statement*> st = codegen(sub, method, lvl, parameters);
         for (auto s : st) {
-            rdata = appendStatement(rdata, s);
-            if (isEOS(s)) rdata = appendBlock(rdata);
+            rdata2 = appendStatement(rdata2, s);
+            if (isEOS(s)) rdata2 = appendBlock(rdata2);
         }
     });
+    return rdata2;
+}
+
+void Compiler::parseMethodDef()
+{
+    PureMethod *new_method = MethodParser(this, item).parse();
+    if (new_method == nullptr) throw "Invalid method!";
+    parent->m_methods[new_method->name()] = new_method;
+    recurse = false;
+    level = 0;
+}
+
+void Compiler::parseStruct()
+{
+    parent->m_structs.push_back(StructParser(item).parse());
+    recurse = false;
+}
+
+void Compiler::parseIndent()
+{
+    int new_level = std::string(item->contents).length();
+    if (!method || new_level == level) return;
+    if (parent->m_blocks.empty()) return;
+
+    method->addBlock(parent->m_blocks);
+    parent->m_blocks = std::vector<std::vector<Statement*>>();
+}
+
+void Compiler::parseMethodCall()
+{
+    rdata.push_back(MethodCallParser(this, item).parse());
+    recurse = false;
+}
+
+void Compiler::parseAssignment()
+{
+    rdata.push_back(AssignmentParser(this, item, method).parse());
+    recurse = false;
+}
+
+void Compiler::parseNumber()
+{
+    rdata.push_back(NumberParser(item).parse());
+}
+
+void Compiler::parseTermOp()
+{
+    rdata.push_back(new Op(item->contents));
+}
+
+Compiler::Compiler()
+{
+    parent = this;
+}
+
+Compiler::Compiler(Compiler *pt, mpc_ast_t *t, PureMethod *m, int l, bool p) :
+    tree(t),
+    method(m),
+    level(l),
+    parameters(p)
+{
+    Compiler *cp = pt;
+    while (cp->parent != cp) {
+        cp = cp->parent;
+    }
+    parent = cp;
+    item = tree;
+}
+
+std::vector<Statement*> Compiler::gen()
+{
+    item = tree;
+    std::string cnts = tree->contents;
+    if (isRoot());
+    else if (isComment()) recurse = false;
+    else if (isMethodDef()) parseMethodDef();
+    else if (isStruct()) parseStruct();
+    else if (isIndent()) parseIndent();
+    else if (isMethodCall()) parseMethodCall();
+    else if (isAssignment()) parseAssignment();
+    else if (isNumber()) parseNumber();
+    else if (isTermOp()) parseTermOp();
+    else if (expect(tree, "factorop")) {
+        rdata.push_back(new Op(cnts));
+    } else if (expect(tree, "string")) {
+        rdata.push_back(new StringValue(cnts));
+    } else if (expect(tree, "typeident")) {
+        TypeIdent *ident = TypeIdentParser(tree).parse();
+        if (parameters) {
+            rdata.push_back(ident);
+        } else if (method != nullptr) {
+            method->addVariable(ident);
+        } else {
+            rdata.push_back(ident);
+        }
+        recurse = false;
+    } else if (expect(tree, "namespacedef")) {
+        if (cnts == "false" || cnts == "true") {
+            rdata.push_back(new Boolean(cnts));
+        } else {
+            NamespaceDef *def = NamespaceDefParser(tree).parse();
+            if (def != nullptr) {
+                rdata.push_back(def);
+            } else if (!cnts.empty()) {
+                rdata.push_back(new Identifier(cnts));
+            }
+        }
+        recurse = false;
+    } else if (expect(tree, "identifier")) {
+        // FIXME Some identifiers are special/reserved words
+        if (cnts == "false" || cnts == "true") {
+            rdata.push_back(new Boolean(cnts));
+        } else {
+            rdata.push_back(new Identifier(cnts));
+        }
+    } else if (expect(tree, "import")) {
+        parent->m_imports.push_back(ImportParser(tree).parse());
+        recurse = false;
+    } else if (expect(tree, "const")) {
+        parent->m_consts.push_back(ConstParser(this, tree).parse());
+        recurse = false;
+    } else if (expect(tree, "newline")) {
+        rdata.push_back(new EOS());
+    } else if (expect(tree, "comparator")) {
+        rdata.push_back(new Comparator(cnts));
+    } else if (expect(tree, "char", "(") || expect(tree, "char", ")")) {
+        rdata.push_back(new Braces(cnts));
+    } else if (expect(tree, "ows") || expect(tree, "ws")) {
+    } else printError("Unknown node in statement", tree);
+
+    if (recurse) {
+        Compiler g(parent, item, method, level, parameters);
+        //rdata = applyToVector<Statement*>(rdata, codegenRecurse(tree, m, level));
+        //rdata = applyToVector<Statement*>(rdata, g.gen());
+        rdata = applyToVector<Statement*>(rdata, g.codegenRecurse(level));
+        /*
+        m_imports = applyToVector<Import*>(m_imports, g.imports());
+        m_consts = applyToVector<Const*>(m_consts, g.consts());
+        m_structs = applyToVector<Struct*>(m_structs, g.structs());
+        for (auto m : g.methods()) {
+            m_methods[m.first] = m.second;
+        }
+        for (auto b : g.blocks()) {
+            m_blocks.push_back(b);
+        }
+        */
+    }
+
     return rdata;
 }
 
-std::vector<Statement*> Compiler::codegen(mpc_ast_t *tree, PureMethod *m, int level, bool parameters)
+std::vector<Statement*> Compiler::codegen(mpc_ast_t *tree, PureMethod *m, int lvl, bool parameters)
 {
-    std::vector<Statement*> rdata;
+    Compiler g(parent, tree, m, lvl, parameters);
+    std::vector<Statement*> res = g.gen();
+    /*
+    m_imports = applyToVector<Import*>(m_imports, g.imports());
+    m_consts = applyToVector<Const*>(m_consts, g.consts());
+    m_structs = applyToVector<Struct*>(m_structs, g.structs());
+    for (auto m : g.methods()) {
+        m_methods[m.first] = m.second;
+    }
+    for (auto b : g.blocks()) {
+        m_blocks.push_back(b);
+    }
+    */
+    return res;
+    /*
+    level = lvl;
+    method = m;
 
+    std::vector<Statement*> rdata;
     std::string tag = tree->tag;
     std::string cnts = tree->contents;
-    bool recurse = true;
+    item = tree;
+    recurse = true;
 
-    if (tag == ">") {
-        //std::cout << "ROOT\n";
-    } else if (expect(tree, "comment")) {
-        // Ignore comments
-        recurse = false;
-    } else if (expect(tree, "methoddef")) {
-        // New method
-        PureMethod *method = MethodParser(this, tree).parse();
-        if (method == nullptr) throw "Invalid method!";
-        m_methods[method->name()] = method;
-        recurse = false;
-        level = 0;
-    } else if (expect(tree, "struct")) {
-        m_structs.push_back(StructParser(tree).parse());
-        recurse = false;
-    } else if (expect(tree, "indent")) {
-        int new_level = cnts.length();
-        if (new_level != level) {
-            if (m && !m_blocks.empty()) {
-                m->addBlock(m_blocks);
-                m_blocks = std::vector<std::vector<Statement*>>();
-            }
-        }
-        // SKIP and recurse
-    } else if (expect(tree, "methodcall")) {
-        rdata.push_back(MethodCallParser(this, tree).parse());
-        recurse = false;
-    } else if (expect(tree, "assignment")) {
-        rdata.push_back(AssignmentParser(this, tree, m).parse());
-        recurse = false;
-    } else if (expect(tree, "number")) {
-        rdata.push_back(NumberParser(tree).parse());
-    } else if (expect(tree, "termop")) {
-        rdata.push_back(new Op(cnts));
-    } else if (expect(tree, "factorop")) {
+    if (isRoot()) ;
+    else if (isComment()) recurse = false;
+    else if (isMethodDef()) parseMethodDef();
+    else if (isStruct()) parseStruct();
+    else if (isIndent()) parseIndent();
+    else if (isMethodCall()) rdata = parseMethodCall(rdata);
+    else if (isAssignment()) rdata = parseAssignment(rdata);
+    else if (isNumber()) rdata = parseNumber(rdata);
+    else if (isTermOp()) rdata = parseTermOp(rdata);
+    else if (expect(tree, "factorop")) {
         rdata.push_back(new Op(cnts));
     } else if (expect(tree, "string")) {
         rdata.push_back(new StringValue(cnts));
@@ -129,8 +269,12 @@ std::vector<Statement*> Compiler::codegen(mpc_ast_t *tree, PureMethod *m, int le
     } else if (expect(tree, "ows") || expect(tree, "ws")) {
     } else printError("Unknown node in statement", tree);
 
-    if (recurse)
-        rdata = applyToVector<Statement*>(rdata, codegenRecurse(tree, m, level));
+    if (recurse) {
+        Compiler g(tree, method, level);
+        rdata = applyToVector<Statement*>(rdata, g.gen());
+        //rdata = applyToVector<Statement*>(rdata, codegenRecurse(tree, m, level));
+    }
 
     return rdata;
+    */
 }
--- src/compiler.hh
+++ src/compiler.hh
@@ -2,6 +2,7 @@
 
 #include <map>
 #include <string>
+#include <iostream>
 #include <3pp/mpc/mpc.h>
 
 #include "statement.hh"
@@ -13,6 +14,7 @@
 #include "struct.hh"
 #include "const.hh"
 #include "import.hh"
+#include "tools.hh"
 
 namespace nolang
 {
@@ -20,23 +22,25 @@ namespace nolang
 class Compiler
 {
 public:
-    Compiler() {}
+    Compiler();
 
     std::vector<Statement*> codegen(mpc_ast_t *tree, PureMethod *m=nullptr, int level=0, bool parameters=false);
 
-    const std::vector<Import*> imports() const { return m_imports; }
-    std::map<std::string, PureMethod*> methods() const { return m_methods; }
-    const std::vector<Const*> consts() const { return m_consts; }
-    const std::vector<Struct*> structs() const { return m_structs; }
-    std::vector<std::vector<Statement*>> blocks() { return m_blocks; }
-    void clearBlocks() { m_blocks.clear(); }
+    const std::vector<Import*> imports() const { return parent->m_imports; }
+    std::map<std::string, PureMethod*> methods() const { return parent->m_methods; }
+    const std::vector<Const*> consts() const { return parent->m_consts; }
+    const std::vector<Struct*> structs() const { return parent->m_structs; }
+    std::vector<std::vector<Statement*>> blocks() { return parent->m_blocks; }
+    void clearBlocks() { parent->m_blocks.clear(); }
 
 protected:
-    bool isEOS(Statement *s) const { return s->type() == "EOS"; }
+    Compiler(Compiler *, mpc_ast_t *, PureMethod *, int level, bool parameters);
+    std::vector<Statement*> gen();
+
     std::vector<Statement*> appendStatement(std::vector<Statement*>, Statement *s);
     std::vector<Statement*> appendBlock(std::vector<Statement*>);
 
-    std::vector<Statement*> codegenRecurse(mpc_ast_t *tree, PureMethod *m, int level);
+    std::vector<Statement*> codegenRecurse(int level);
 
     std::map<std::string, PureMethod*> m_methods;
     std::vector<std::vector<Statement*>> m_blocks;
@@ -44,7 +48,35 @@ protected:
     std::vector<Const*> m_consts;
     std::vector<Struct*> m_structs;
 
-    std::string m_last_indent;
+    bool isEOS(Statement *s) const { return s->type() == "EOS"; }
+    bool isRoot() const { return std::string(item->tag) == ">"; }
+    bool isComment() const { return expect(item, "comment"); }
+    bool isMethodDef() const { return expect(item, "methoddef"); }
+    bool isMethodCall() const { return expect(item, "methodcall"); }
+    bool isStruct() const { return expect(item, "struct"); }
+    bool isIndent() const { return expect(item, "indent"); }
+    bool isAssignment() const { return expect(item, "assignment"); }
+    bool isNumber() const { return expect(item, "number"); }
+    bool isTermOp() const { return expect(item, "termop"); }
+
+    void parseMethodDef();
+    void parseMethodCall();
+    void parseStruct();
+    void parseIndent();
+    void parseAssignment();
+    void parseNumber();
+    void parseTermOp();
+
+    Compiler *parent;
+    mpc_ast_t *tree;
+    mpc_ast_t *item;
+    PureMethod *method;
+
+    int level;
+    bool recurse;
+    bool parameters;
+
+    std::vector<Statement*> rdata;
 };
 
 }
