412853a2accb10cf19c67b60920c51acf05bc42b
Jouni Roivas <jroivas@iki.fi>
2017-07-02 00:45:12 +0300

    Cleanups

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -167,20 +167,7 @@ std::vector<Statement*> Compiler::gen()
 
     if (recurse) {
         Compiler g(parent, item, method, level, parameters);
-        //rdata = applyToVector<Statement*>(rdata, codegenRecurse(tree, m, level));
-        //rdata = applyToVector<Statement*>(rdata, g.gen());
         rdata = applyToVector<Statement*>(rdata, g.codegenRecurse(level));
-        /*
-        m_imports = applyToVector<Import*>(m_imports, g.imports());
-        m_consts = applyToVector<Const*>(m_consts, g.consts());
-        m_structs = applyToVector<Struct*>(m_structs, g.structs());
-        for (auto m : g.methods()) {
-            m_methods[m.first] = m.second;
-        }
-        for (auto b : g.blocks()) {
-            m_blocks.push_back(b);
-        }
-        */
     }
 
     return rdata;
@@ -190,91 +177,5 @@ std::vector<Statement*> Compiler::codegen(mpc_ast_t *tree, PureMethod *m, int lv
 {
     Compiler g(parent, tree, m, lvl, parameters);
     std::vector<Statement*> res = g.gen();
-    /*
-    m_imports = applyToVector<Import*>(m_imports, g.imports());
-    m_consts = applyToVector<Const*>(m_consts, g.consts());
-    m_structs = applyToVector<Struct*>(m_structs, g.structs());
-    for (auto m : g.methods()) {
-        m_methods[m.first] = m.second;
-    }
-    for (auto b : g.blocks()) {
-        m_blocks.push_back(b);
-    }
-    */
     return res;
-    /*
-    level = lvl;
-    method = m;
-
-    std::vector<Statement*> rdata;
-    std::string tag = tree->tag;
-    std::string cnts = tree->contents;
-    item = tree;
-    recurse = true;
-
-    if (isRoot()) ;
-    else if (isComment()) recurse = false;
-    else if (isMethodDef()) parseMethodDef();
-    else if (isStruct()) parseStruct();
-    else if (isIndent()) parseIndent();
-    else if (isMethodCall()) rdata = parseMethodCall(rdata);
-    else if (isAssignment()) rdata = parseAssignment(rdata);
-    else if (isNumber()) rdata = parseNumber(rdata);
-    else if (isTermOp()) rdata = parseTermOp(rdata);
-    else if (expect(tree, "factorop")) {
-        rdata.push_back(new Op(cnts));
-    } else if (expect(tree, "string")) {
-        rdata.push_back(new StringValue(cnts));
-    } else if (expect(tree, "typeident")) {
-        TypeIdent *ident = TypeIdentParser(tree).parse();
-        if (parameters) {
-            rdata.push_back(ident);
-        } else if (m != nullptr) {
-            m->addVariable(ident);
-        } else {
-            rdata.push_back(ident);
-        }
-        recurse = false;
-    } else if (expect(tree, "namespacedef")) {
-        if (cnts == "false" || cnts == "true") {
-            rdata.push_back(new Boolean(cnts));
-        } else {
-            NamespaceDef *def = NamespaceDefParser(tree).parse();
-            if (def != nullptr) {
-                rdata.push_back(def);
-            } else if (!cnts.empty()) {
-                rdata.push_back(new Identifier(cnts));
-            }
-        }
-        recurse = false;
-    } else if (expect(tree, "identifier")) {
-        // FIXME Some identifiers are special/reserved words
-        if (cnts == "false" || cnts == "true") {
-            rdata.push_back(new Boolean(cnts));
-        } else {
-            rdata.push_back(new Identifier(cnts));
-        }
-    } else if (expect(tree, "import")) {
-        m_imports.push_back(ImportParser(tree).parse());
-        recurse = false;
-    } else if (expect(tree, "const")) {
-        m_consts.push_back(ConstParser(this, tree).parse());
-        recurse = false;
-    } else if (expect(tree, "newline")) {
-        rdata.push_back(new EOS());
-    } else if (expect(tree, "comparator")) {
-        rdata.push_back(new Comparator(cnts));
-    } else if (expect(tree, "char", "(") || expect(tree, "char", ")")) {
-        rdata.push_back(new Braces(cnts));
-    } else if (expect(tree, "ows") || expect(tree, "ws")) {
-    } else printError("Unknown node in statement", tree);
-
-    if (recurse) {
-        Compiler g(tree, method, level);
-        rdata = applyToVector<Statement*>(rdata, g.gen());
-        //rdata = applyToVector<Statement*>(rdata, codegenRecurse(tree, m, level));
-    }
-
-    return rdata;
-    */
 }
