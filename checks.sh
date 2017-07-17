7e2d2f601033e96015f0dbd38a4985b86bcc8c0b
Jouni Roivas <jroivas@iki.fi>
2017-07-18 00:04:27 +0300

    Couple fixes for 3pp

--- checks.sh
+++ checks.sh
@@ -2,4 +2,5 @@
 
 MYDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-cppcheck --error-exitcode=1 "${MYDIR}/3pp" "${MYDIR}/src"
+#cppcheck --error-exitcode=1 "${MYDIR}/3pp" "${MYDIR}/src"
+cppcheck --error-exitcode=1 "${MYDIR}/src"
