3d5f3476ac3eb1281c8b3334202abc0ea5d47765
Jouni Roivas <jroivas@iki.fi>
2017-07-19 13:20:37 +0300

    Start to separate native type solver

--- src/codegen/typesolver.cpp
+++ src/codegen/typesolver.cpp
@@ -81,8 +81,85 @@ TypeIdent *TypeSolver::solveVariable(const std::string &name) const
     return nullptr;
 }
 
+std::string NativeTypeSolver::solveTypeDef() const
+{
+    std::string t = statement->code();
+    if (t == "void") return "void";
+    return solver->native(t);
+}
+
+std::string NativeTypeSolver::solveTypeIdent() const
+{
+    const TypeIdent *i = static_cast<const TypeIdent *>(statement);
+    std::string n = solver->native(i->varType());
+    // FIXME
+    std::cerr << "*** INVALID TYPEIDENT\n";
+    return "";
+}
+
+std::string NativeTypeSolver::solveIdentifier() const
+{
+    TypeIdent *var = solver->solveVariable(statement->code());
+    if (var) return solver->native(var->varType());
+    return "invalid";
+}
+
+std::string NativeTypeSolver::solveString() const
+{
+    return "char *";
+}
+
+double NativeTypeSolver::convertToDouble(const std::string &num) const
+{
+    try {
+        return std::stod(num, nullptr);
+    }
+    catch (std::out_of_range r) {
+        throw "Invalid number: " + num;
+    }
+}
+
+std::string NativeTypeSolver::solveNegativeNumber(double value) const
+{
+    if (fabs(value) >= std::numeric_limits<int32_t>::max()) return "int64_t";
+    return "int32_t";
+}
+
+std::string NativeTypeSolver::solvePositiveNumber(double value) const
+{
+    if (value >= std::numeric_limits<int32_t>::max()) return "uint64_t";
+    return "uint32_t";
+}
+
+std::string NativeTypeSolver::solveNumber() const
+{
+    std::string num = statement->code();
+    num = trim(num);
+    /* TODO FIXME binary
+    if (num.substr(0,2) == "0b") {
+        // TODO Convert binary to hex
+    }
+    */
+    double value = convertToDouble(num);
+    if (isNegativeNumber(num)) return solveNegativeNumber(value);
+    return solvePositiveNumber(value);
+}
+
+std::string NativeTypeSolver::solve() const
+{
+    if (isTypeDef()) return solveTypeDef();
+    else if (isTypeIdent()) return solveTypeIdent();
+    else if (isIdentifier()) return solveIdentifier();
+    else if (isString()) return solveString();
+    else if (isNumber()) return solveNumber();
+    return "";
+}
+
 std::string TypeSolver::native(const Statement *s) const
 {
+    return NativeTypeSolver(this, s).solve();
+
+    // XXX
     if (s->type() == "TypeDef") {
         if (s->code() == "void") {
             return "void";
--- src/codegen/typesolver.hh
+++ src/codegen/typesolver.hh
@@ -25,5 +25,35 @@ private:
     const PureMethod *method;
 };
 
+class NativeTypeSolver
+{
+public:
+    NativeTypeSolver(const TypeSolver *t, const Statement *s) :
+        solver(t),
+        statement(s) {}
+    std::string solve() const;
+
+private:
+    bool isTypeDef() const { return statement->type() == "TypeDef"; }
+    bool isTypeIdent() const { return statement->type() == "TypeIdent"; }
+    bool isIdentifier() const { return statement->type() == "Identifier"; }
+    bool isString() const { return statement->type() == "String" || statement->type() == "string"; }
+    bool isNumber() const { return statement->type() == "Number"; }
+    bool isNegativeNumber(const std::string &n) const { return n[0] == '-'; }
+
+    std::string solveTypeDef() const;
+    std::string solveTypeIdent() const;
+    std::string solveIdentifier() const;
+    std::string solveString() const;
+    std::string solveNumber() const;
+    std::string solveNegativeNumber(double) const;
+    std::string solvePositiveNumber(double) const;
+
+    double convertToDouble(const std::string &) const;
+
+    const TypeSolver *solver;
+    const Statement *statement;
+};
+
 }
 
